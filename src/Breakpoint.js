28f1910afd7dcb3f455241550f8335b4f2e182fc
Brent Jackson <jxnblk@gmail.com>
2017-04-20 10:15:05 -0400

    Remove placeholder color from breakpoint icon

--- src/Breakpoint.js
+++ src/Breakpoint.js
@@ -1,23 +1,18 @@
 
 const React = require('react')
-const chroma = require('chroma-js')
 const Logo = require('./Logo')
-
-const blue = '#00badb'
-const color = blue
-const bg = chroma(blue).darken(3.5).desaturate(1/2)
 
 module.exports = ({
   size = 1024
 }) => {
   return (
     <Logo
-      color={color}
-      backgroundColor={bg}
+      color='white'
+      backgroundColor='black'
       size={size}
       radius={1}
       horizontal>
-      <circle r={1} fill={bg} />
+      <circle r={1} fill='black' />
     </Logo>
   )
 }
